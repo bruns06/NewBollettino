3110479c72faf198617234f2bbf0d4a06fe921b4
bruns2506 <bruns2506@gmail.com>
2023-07-04 22:51:33 +0200

    changed html

--- src/main/java/com/bollettino/presentation/ControllerMVC.java
+++ src/main/java/com/bollettino/presentation/ControllerMVC.java
@@ -6,21 +6,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
-
 import com.bollettino.entities.Bollettino;
 import com.bollettino.repo.BollettinoDAO;
 import com.bollettino.service.BollettinoService;
 
-import jakarta.servlet.http.HttpSession;
 
 @Controller
 @SessionAttributes({"CodiceBollettino", "CCDestinatario", "Causale", "Importo"})
 @RequestMapping("/")
 public class ControllerMVC {
-	
-	int id = 0;
 	
 	@Autowired
 	BollettinoService service;
@@ -41,7 +36,7 @@ public class ControllerMVC {
 	@PostMapping("/pagamento-bollettino")
 	public String pagamentoBollettino(Bollettino b, Model m) {
 		service.addDatiBollettino(b);
-		id = b.getId();		
+			
 		m.addAttribute("CodiceBollettino", b.getCodiceBollettino());
 		m.addAttribute("CCDestinatario", b.getCodiceContoDestinatario());
 		m.addAttribute("Causale", b.getCausale());
@@ -49,12 +44,9 @@ public class ControllerMVC {
 		
 		return "pagamento-bollettino";
 	}
-	/*@GetMapping("/pagamento-bollettino")
-	public String pagamentoBollettino(@ModelAttribute Bollettino formBolletino, Model model) {
-		return "pagamento-bollettino";
-	}*/
+
 	@PostMapping("/conferma-pagamento")
-	public String confermaPagamento(Model m, Bollettino b, HttpSession httpSession) {	
+	public String confermaPagamento(Model m, Bollettino b) {	
 		
 		m.addAttribute("NomePaga", b.getNomePagatore());
 		m.addAttribute("CognomePaga", b.getCognomePagatore());
@@ -62,11 +54,4 @@ public class ControllerMVC {
 		
 		return "conferma-pagamento";
 	}
-	
-	 //@PostMapping("/testi")
-	 	//public String addTesto(Bollettino b) {
-		 //bs.pagaBollettino(b);
-		 //return "redirect:/review";
-	 //}
-	
 }
